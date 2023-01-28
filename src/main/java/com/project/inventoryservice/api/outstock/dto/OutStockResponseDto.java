fd27c2ae691170927c570c845cd07935faeec7b3
chaelin <nias0327@naver.com>
2023-01-29 01:10:55 +0900

    fix : categoryCode로 조회하는 오류 수정

--- src/main/java/com/project/inventoryservice/api/outstock/dto/OutStockResponseDto.java
+++ src/main/java/com/project/inventoryservice/api/outstock/dto/OutStockResponseDto.java
@@ -1,6 +1,7 @@
 package com.project.inventoryservice.api.outstock.dto;
 
 import com.project.inventoryservice.domain.outstock.OutStock;
+import com.project.inventoryservice.domain.product.Category;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,7 +34,7 @@ public class OutStockResponseDto {
     @Builder
     public OutStockResponseDto(OutStock entity) {
         this.id = entity.getId();
-        this.categoryName = entity.getProduct().getCategoryCode();
+        this.categoryName = Category.findByKey(entity.getProduct().getCategoryCode()).getName();
         this.productId =  entity.getProduct().getId();
         this.productCode = entity.getProduct().getProductCode();
         this.productName = entity.getProduct().getProductName();
