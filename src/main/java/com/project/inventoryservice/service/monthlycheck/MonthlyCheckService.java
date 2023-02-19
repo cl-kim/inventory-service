b0295a5fdbb918817e7a35e8a5f23084ff32a4ad
ChaeLin <nias03@naver.com>
2023-02-19 22:50:52 +0900

    fix : 마감취소 오류 개선

--- src/main/java/com/project/inventoryservice/service/monthlycheck/MonthlyCheckService.java
+++ src/main/java/com/project/inventoryservice/service/monthlycheck/MonthlyCheckService.java
@@ -14,34 +14,49 @@ import java.time.LocalDate;
 public class MonthlyCheckService {
     private final MonthlyCheckRepository monthlyCheckRepository;
 
-    public Boolean checkMonthlyEnd(LocalDate date){
+    public Boolean checkMonthlyEnd(LocalDate date) {
         MonthlyCheck entity = monthlyCheckRepository.findByEndMonth(date)
                 .orElse(MonthlyCheck.builder().isEnd(false).build());
         return entity.getIsEnd();
     }
 
+    @Transactional
     public Boolean saveMonthlyEnd(LocalDate date) {
         LocalDate lastMonth = date.minusMonths(1L);
-        if(!monthlyCheckRepository.findByEndMonth(lastMonth).isPresent()){
+
+        // 전월 마감이 존재하고
+        if (monthlyCheckRepository.findByEndMonth(lastMonth).isPresent()) {
+            // 전월 마감이 true일때만 이번달 마감이 가능하다.
+            if (monthlyCheckRepository.findByEndMonth(lastMonth).get().getIsEnd()) {
+                saveEnd(date);
+            } else {
+                throw new RuntimeException("전월 마감이 필요합니다.");
+            }
+        } else {
             throw new RuntimeException("전월 마감이 필요합니다.");
         }
-        saveEnd(date);
         return true;
     }
 
+    @Transactional
     public Boolean saveFirstMonthlyEnd(LocalDate date) {
         LocalDate lastMonth = date.minusMonths(1L);
         saveEnd(lastMonth);
         return true;
     }
 
+    @Transactional
     public Boolean cancelMonthlyEnd(LocalDate date) {
-        MonthlyCheck entity =  monthlyCheckRepository.findByEndMonth(date).get();
-        entity.updateEnd(false);
+        MonthlyCheck entity = monthlyCheckRepository.findByEndMonth(date).get();
+        monthlyCheckRepository.delete(entity);
         return true;
     }
 
-    public void saveEnd(LocalDate date){
+    @Transactional
+    public void saveEnd(LocalDate date) {
+        if(monthlyCheckRepository.findByEndMonth(date).isPresent()){
+            throw new RuntimeException("이미 마감된 월입니다.");
+        }
         MonthlyCheck entity = MonthlyCheck.builder()
                 .endMonth(date)
                 .isEnd(true)
