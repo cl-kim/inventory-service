d63bb18a099858cc1ac5951e0a0373fa7a451a71
chaelin <nias0327@naver.com>
2023-02-25 10:34:16 +0900

    삭제된 파일

--- src/main/java/com/project/inventoryservice/domain/instock/InStockRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.inventoryservice.domain.instock;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InStockRepository extends JpaRepository<InStock, Long>, InStockRepositoryCustom {
-}
--- src/main/java/com/project/inventoryservice/domain/outstock/OutStockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.inventoryservice.domain.outstock;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OutStockRepository extends JpaRepository<OutStock, Long>, OutStockRepositoryCustom {
-
-
-}
