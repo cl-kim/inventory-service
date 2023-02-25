04cc8ff26a9630f4c87eddd99f5d703d9c9a04a5
chaelin <nias0327@naver.com>
2023-02-25 10:40:33 +0900

    add StockResponseDto

---
+++ src/main/java/com/project/inventoryservice/api/inventory/dto/StockResponseDto.java
@@ -0,0 +1,32 @@
+package com.project.inventoryservice.api.inventory.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class StockResponseDto {
+
+    private Integer currentMonthStock;
+    private Integer currentMonthOutStock;
+    private Integer currentMonthInStock;
+    private Integer lastMonthStock;
+    private Integer lastMonthOutStock;
+    private Integer lastMonthInStock;
+    private String productCode;
+    private String productName;
+
+    public StockResponseDto(Integer currentMonthStock, Integer currentMonthOutStock, Integer currentMonthInStock,
+                            Integer lastMonthStock, Integer lastMonthOutStock, Integer lastMonthInStock,
+                            String productCode, String productName) {
+        this.currentMonthStock = currentMonthStock;
+        this.currentMonthOutStock = currentMonthOutStock;
+        this.currentMonthInStock = currentMonthInStock;
+        this.lastMonthStock = lastMonthStock;
+        this.lastMonthOutStock = lastMonthOutStock;
+        this.lastMonthInStock = lastMonthInStock;
+        this.productCode = productCode;
+        this.productName = productName;
+    }
+}
